a0783fd16bdc4fff529409a652a3fc893baf1a17
Vibrissa <zzozzozz15@gmail.com>
2021-06-21 13:47:16 +0800

    feat: canvas layer upon video

--- src/App.tsx
+++ src/App.tsx
@@ -9,50 +9,62 @@ function App() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const drawVideo = React.useCallback(async () => {
-    const ctx = canvasRef.current?.getContext("2d");
-
-    if (ctx) {
-      ctx.drawImage(videoRef.current!, 0, 0, 640, 480);
-      const imageData = ctx.getImageData(0, 0, 640, 480);
-      ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-      const res = await scanImageData(imageData);
-      if (res.length > 0) {
-        const points = res[0].points;
-        ctx.beginPath();
-        ctx.arc(points[0].x, points[0].y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(points[1].x, points[1].y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(points[2].x, points[2].y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.arc(points[3].x, points[3].y, 10, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.font = "25px Arial";
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillText(res[0].decode(), 10, 50);
+    if (canvasRef.current && videoRef.current) {
+      const canvasLayer = canvasRef.current;
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const layerCtx = canvasLayer.getContext("2d");
+      const canvasWidth = videoRef.current.offsetWidth;
+      const canvasHeight = videoRef.current.offsetHeight;
+      canvasLayer.width = canvasWidth;
+      canvasLayer.height = canvasHeight;
+      canvas.width = canvasWidth;
+      canvas.height = canvasHeight;
+      if (ctx && layerCtx) {
+        ctx.drawImage(videoRef.current!, 0, 0, canvasWidth, canvasHeight);
+        const imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+        const res = await scanImageData(imageData);
+        if (res.length > 0) {
+          layerCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+          const points = res[0].points;
+          layerCtx.beginPath();
+          layerCtx.arc(points[0].x, points[0].y, 10, 0, 2 * Math.PI);
+          layerCtx.stroke();
+          layerCtx.beginPath();
+          layerCtx.arc(points[1].x, points[1].y, 10, 0, 2 * Math.PI);
+          layerCtx.stroke();
+          layerCtx.beginPath();
+          layerCtx.arc(points[2].x, points[2].y, 10, 0, 2 * Math.PI);
+          layerCtx.stroke();
+          layerCtx.beginPath();
+          layerCtx.arc(points[3].x, points[3].y, 10, 0, 2 * Math.PI);
+          layerCtx.stroke();
+          layerCtx.font = "25px Arial";
+          layerCtx.fillStyle = "#FFFFFF";
+          layerCtx.fillText(res[0].decode(), 10, 50);
+        } else {
+          layerCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+        }
       }
     }
-    requestAnimationFrame(drawVideo);
   }, []);
   React.useLayoutEffect(() => {
+    let interval: NodeJS.Timeout;
     if (videoRef.current && canvasRef.current) {
-      videoStream(videoRef.current);
-
-      canvasRef.current.width = 640;
-      canvasRef.current.height = 480;
-      requestAnimationFrame(drawVideo);
+      videoStream(videoRef.current, canvasRef.current);
+      interval = setInterval(drawVideo, 16);
     }
+    return () => {
+      clearInterval(interval);
+    };
   }, [drawVideo]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <video playsInline autoPlay ref={videoRef} />
-        <canvas ref={canvasRef} />
-      </header>
+      <div className="scanner-wrapper">
+        <video className="video-box" playsInline autoPlay muted controls={false} ref={videoRef} />
+        <canvas className="canvas-layer" ref={canvasRef} />
+      </div>
     </div>
   );
 }
--- src/utils/videoStream.ts
+++ src/utils/videoStream.ts
@@ -1,11 +1,14 @@
 const constraints = {
   audio: false,
-  video: true,
+  video: { facingMode: "environment" },
 };
 
-function handleSuccessWithVideo(video: HTMLVideoElement) {
+function handleSuccessWithVideo(video: HTMLVideoElement, canvas: HTMLCanvasElement) {
   return (stream: MediaStream) => {
     video.srcObject = stream;
+    const { width, height } = stream.getTracks()[0].getSettings();
+    canvas.width = width ?? 0;
+    canvas.height = height ?? 0;
   };
 }
 
@@ -13,7 +16,7 @@ function handleError(error: any) {
   console.log("navigator.MediaDevices.getUserMedia error: ", error.message, error.name);
 }
 
-export const videoStream = (video: HTMLVideoElement) => {
-  const handleSuccess = handleSuccessWithVideo(video);
+export const videoStream = (video: HTMLVideoElement, canvas: HTMLCanvasElement) => {
+  const handleSuccess = handleSuccessWithVideo(video, canvas);
   navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 };
